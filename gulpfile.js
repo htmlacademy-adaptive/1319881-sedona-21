b7b00bf36a463a18b8243128ded0ef81cfb02b4b
katcos <katekaray@gmail.com>
2021-01-25 20:19:37 +0300

    АвтоматизирÐует сборку личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const rename = require("gulp-rename");
+const csso = require("postcss-csso");
+const uglify = require("gulp-uglify-es").default;
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
 const sync = require("browser-sync").create();
+const del = require("del");
 
 // Styles
 
@@ -16,14 +22,63 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/app.js")
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream())
+}
+
+exports.scripts = scripts;
+
+// Html
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("build"))
+    .pipe(sync.stream());
+}
+
+exports.html = html;
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = images;
+
+// Webp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
 
 // Sprite
 
@@ -31,17 +86,41 @@ const sprite = () => {
   return gulp.src("source/img/icons/*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img/icons"))
+    .pipe(gulp.dest("build/img"))
 }
 
 exports.sprite = sprite;
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+      "source/fonts/*.{woff2,woff}",
+      "source/**/*.ico",
+      "source/img/**/*.{jpg,png,svg}"
+    ],
+    {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"))
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -56,9 +135,41 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/js/**/*.js", gulp.series("scripts"));
+  gulp.watch("source/*.html", gulp.series("html", sync.reload));
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
+// Build
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    scripts,
+    html,
+    sprite,
+    copy,
+    images,
+    createWebp
+  )
 );
+
+exports.build = build;
+
+// Default
+
+exports.default = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    scripts,
+    html,
+    sprite,
+    copy,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
+)
---
+++ source/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+  let pageNav = document.querySelector('.page-nav');
+  let pageNavToggle = document.querySelector('.page-nav__toggle');
+
+  pageNavToggle.addEventListener('click', function () {
+    pageNav.classList.toggle('page-nav--show');
+  });
+})();
+
+document.addEventListener("DOMContentLoaded", function () {
+  (function () {
+    let map = document.querySelector("#map");
+    if (map === null) {
+      return;
+    }
+
+    ymaps.ready(init);
+
+    function init() {
+      const coord = [34.869726184622785, -111.76009820576179];
+      let myMap = new ymaps.Map("map", {
+        center: coord,
+        zoom: 10
+      });
+      let myPlacemark = new ymaps.Placemark(coord, {
+          hintContent: "город Седона"
+        }, {
+          iconLayout: "default#image",
+          iconImageHref: "img/map-marker.svg",
+          iconImageSize: [27, 27],
+          iconImageOffset: [-13, -13]
+        }
+      );
+      myMap.geoObjects.add(myPlacemark);
+    }
+
+    //Hide map image, if map container was found
+    //document.querySelector(".page-footer__map-image").classList.add("visually-hidden");
+  })();
+});
